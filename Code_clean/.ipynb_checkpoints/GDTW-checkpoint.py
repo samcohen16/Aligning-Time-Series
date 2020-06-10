6447b7bc59fc8aa1b26aa42e439611a588eaa138
samcohen16 <samuel.cohen.19@ucl.ac.uk>
2020-06-10 17:08:19 +0300

    first commit

---
+++ Code_clean/.ipynb_checkpoints/GDTW-checkpoint.py
@@ -0,0 +1,168 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable, grad, Function
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from numba import jit
+
+from itertools import product
+
+from .DTW import dtw, _SoftDTW
+
+
+class gromov_dtw(nn.Module):
+    def __init__(self, max_iter, gamma = 0.5, loss_only = 0, device = 'cpu',
+                 dtw_approach = 'GDTW', verbose = 1, tol = 1e-3):
+        
+        super(gromov_dtw, self).__init__()
+
+        self.max_iter = max_iter
+        self.device = device
+        self.gamma = gamma
+        self.dtw_approach = dtw_approach
+        self.verbose = verbose
+        self.loss_only = loss_only
+        self.losses_gdtw = []
+        self.tol=tol
+
+
+    def forward(self, x, y, xteps=1e-3):
+        #### Computes GDTW and returns the optimal alignment matrix and optimal path
+        # 
+        
+        #Computes cost matrices C_x and C_y
+        C1 = self._cost_matrix(x, x)
+        C2 = self._cost_matrix(y, y)
+
+        if self.loss_only:
+                
+                loss = self.g_dtw(C1,C2)
+                return loss
+        else:
+                loss, A = self.g_dtw(C1, C2)
+
+        return( loss, A )
+
+    
+    def init_alignment(self,C1,C2):
+        ###  Initializes the alignment matrix to the identity plus extra ones on the sides to make it a proper alignment
+        
+        A = torch.eye(C1.shape[0], C2.shape[0]).float()
+        
+        ### If alignment is rectangular - add ones to make it a proper alignment along the largest dimension
+        if C1.shape[0] > C2.shape[0]:
+            A[C2.shape[0]:,-1] = 1
+            
+        elif C2.shape[0] > C1.shape[0]:
+            A[-1,C1.shape[0]:] = 1
+        
+        return(A)
+    
+    def _cost_matrix(self, x, y, p=2):
+        ###  Computes a cost matrix given two tensors
+
+        x_col = x.unsqueeze(-2)
+        y_lin = y.unsqueeze(-3)
+
+
+        C = torch.sum((torch.abs(x_col - y_lin)) ** p, -1)
+
+        return C.float()
+
+    def compute_constc(self, f1C1, f2C2, A):
+        ###  Computes terms in the tensor contraction given the cost matrices and the current alignment
+
+        p = torch.sum(A, axis=1).reshape(-1, 1)
+        q = torch.sum(A, axis=0).reshape(-1, 1)
+
+        ones_m = torch.ones(len(p)).float().reshape(-1,1)
+        ones_n = torch.ones(len(q)).float().reshape(-1,1)
+
+        constC1 = torch.matmul(torch.matmul(f1C1, p), ones_n.T)
+        constC2 = torch.matmul(ones_m, torch.matmul(q.T, f2C2.T))
+
+        constC = constC1 + constC2
+        
+        return(constC)
+
+
+    def tensor_product(self, f1C1, f2C2, hC1, hC2, A):
+        ###  Computes the tensor contraction   $L \otimes A$    given the current alignment 
+        
+        constC = self.compute_constc(f1C1, f2C2, A)
+        
+        AB = - torch.sparse.mm(A.T.to_sparse(), hC1.T).T.matmul(hC2.T)
+        
+        tens = constC + AB
+        
+        return tens
+    
+        
+    def compute_dtw_alignment(self, tens):
+        ###  Computes the DTW alignment for the cost $ D = L \otimes A $
+        
+        if self.dtw_approach == 'GDTW':
+                A = dtw(tens)
+
+        elif self.dtw_approach == 'soft_GDTW':
+                if not tens.requires_grad:
+                    tens = torch.autograd.Variable(tens, requires_grad = True)
+                    
+                tens = tens.reshape(1, tens.shape[0], tens.shape[1])
+
+                sdtw_pyt = _SoftDTW()
+                
+                loss = sdtw_pyt.apply(tens, self.gamma)
+
+                A = torch.autograd.grad(loss, tens, create_graph=True)[0][0]   
+                
+        
+        return(A)
+    
+    def g_dtw(self,C1, C2):
+        ### Computes GDTW, the optimal alignment matrix and optimal path
+        
+        #Initialize the alignment matrix A
+        A = self.init_alignment(C1, C2)
+        
+        #Compute terms of the tensor contraction
+        hC1, hC2 = C1, 2*C2
+        f1C1, f2C2 = C1**2, C2**2
+
+        cpt = 0
+        err=1
+
+        while (err > self.tol and cpt < self.max_iter):
+    
+            # Compute tensor contraction $ tens = L \otimes A $
+            tens = self.tensor_product(f1C1, f2C2, hC1, hC2, A)
+            
+            # Computes $ < L \otimes A, A > $, i.e. current value of the GDTW functional 
+            self.losses_gdtw.append( torch.sum(tens * A) )
+            self.A = A
+            # Perform linear minimization oracle step  $\argmin_{A'} < L \otimes A, A' > $
+            A = self.compute_dtw_alignment(tens)
+
+            
+            if cpt>0:
+                err = self.losses_gdtw[-2] - self.losses_gdtw[-1] 
+                
+            if self.verbose:
+                print('iter:', cpt, 'GDTW:', self.losses_gdtw[-1].item())
+                
+                if err<0:
+                    print('FW step-size = 0')
+            
+        
+
+            cpt += 1
+   
+
+        if self.loss_only:
+                return self.losses_gdtw[-2]
+        else:
+                return self.losses_gdtw[-2], self.A
+
+
---
+++ Code_clean/DTW.py
@@ -0,0 +1,135 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable, grad, Function
+import math
+import numpy as np
+from numba import jit
+from itertools import product
+
+
+def dtw(D):
+    ######### Given a distance matrix D_xy, computes DTW and the optimal alignment matrix A
+    ### Input: D, dim: T_x x T_y
+    ### Output A, dim: T_x x T_y
+    
+        m=D.shape[0]
+        n=D.shape[1]
+
+        R=torch.zeros((m+1,n+1))
+        R[1:,0]=np.inf
+        R[0,1:]=np.inf
+
+        ### Forward recursion to compute DTW. The value of DTW correspongs to R[-1,-1] ###
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                R[i,j]=D[i-1,j-1]+np.min((R[i,j-1],R[i-1,j],R[i-1,j-1]))
+
+        A=torch.zeros((m,n))
+        k,l = m-1,n-1
+        A[0,0]=A[k,l]=1
+        
+               ### Backward recursion to compute the optimal alignment matrix A ###
+        while (k>=0 and l>=0):
+            
+            move = np.argmin((R[k,l+1],R[k+1,l],R[k,l]))
+            if move==2:
+                A[k-1,l-1]=1
+                k=k-1
+                l=l-1
+            elif move==0:
+                A[k-1,l]=1
+                k=k-1
+
+            elif move==1:
+                A[k,l-1]=1
+                l=l-1
+
+
+        return(A)
+    
+@jit(nopython = True)
+def compute_softdtw(D, gamma):
+        ######### Given a distance matrix D_xy, and entropic coefficient \gamma, computes soft-DTW forward
+        ### Input: D, dim: n_batches x T_x x T_y
+        ###        gamma, dim: 1
+        ### Output R, dim: n_batches x T_x x T_y
+        B = D.shape[0]
+        N = D.shape[1]
+        M = D.shape[2]
+        R = np.ones((B, N + 2, M + 2)) * np.inf
+        R[:, 0, 0] = 0
+        for k in range(B):
+            for j in range(1, M + 1):
+                for i in range(1, N + 1):
+                    r0 = -R[k, i - 1, j - 1] / gamma
+                    r1 = -R[k, i - 1, j] / gamma
+                    r2 = -R[k, i, j - 1] / gamma
+                    rmax = max(max(r0, r1), r2)
+                    rsum = np.exp(r0 - rmax) + np.exp(r1 - rmax) + np.exp(r2 - rmax)
+                    softmin = - gamma * (np.log(rsum) + rmax)
+                    R[k, i, j] = D[k, i - 1, j - 1] + softmin
+        return R
+
+@jit(nopython = True)
+def compute_softdtw_backward(D_, R, gamma):
+    ######### Given a distance matrix D_xy, forward values in the forward recursion of DTW and entropic coefficient \gamma, performs a backward pass, which computes the optimal soft-alignment matrix A
+    
+        ### Input: D, dim: n_batches x T_x x T_y
+        ###        R, dim: n_batches x T_x x T_y
+        ###        gamma, dim: 1
+        ### Output A, dim: n_batches x T_x x T_y
+        
+    B = D_.shape[0]
+    N = D_.shape[1]
+    M = D_.shape[2]
+    D = np.zeros((B, N + 2, M + 2))
+    E = np.zeros((B, N + 2, M + 2))
+    D[:, 1:N + 1, 1:M + 1] = D_
+    E[:, -1, -1] = 1
+    R[:, : , -1] = -np.inf
+    R[:, -1, :] = -np.inf
+    R[:, -1, -1] = R[:, -2, -2]
+    for k in range(B):
+        for j in range(M, 0, -1):
+            for i in range(N, 0, -1):
+                a0 = (R[k, i + 1, j] - R[k, i, j] - D[k, i + 1, j]) / gamma
+                b0 = (R[k, i, j + 1] - R[k, i, j] - D[k, i, j + 1]) / gamma
+                c0 = (R[k, i + 1, j + 1] - R[k, i, j] - D[k, i + 1, j + 1]) / gamma
+                a = np.exp(a0)
+                b = np.exp(b0)
+                c = np.exp(c0)
+                E[k, i, j] = E[k, i + 1, j] * a + E[k, i, j + 1] * b + E[k, i + 1, j + 1] * c
+    return E[:, 1:N + 1, 1:M + 1]
+
+
+class _SoftDTW(Function):
+    
+        @staticmethod
+        def forward(ctx, D, gamma):
+            ######### Forward pass computing soft-DTW given a distance matrix D and an entropic coefficient gamma
+    
+        ### Input: D, dim: n_batches x T_x x T_y
+        ###        gamma, dim: 1
+        ### Output R, dim: n_batches x T_x x T_y
+
+                gamma = torch.Tensor([gamma]) # dtype fixed
+                D_ = D.detach().cpu().numpy()
+                g_ = gamma.item()
+                R=compute_softdtw(D_, g_)
+                R = torch.tensor(R, dtype=torch.double)
+                ctx.save_for_backward(D, R, gamma)
+                return R[:, -2, -2]
+
+        @staticmethod
+        def backward(ctx, grad_output):
+             ######### Backward pass computing the soft-alignment matrix
+
+        ### Output E, dim: n_batches x T_x x T_y
+                dev = grad_output.device
+                dtype = grad_output.dtype
+                D, R, gamma = ctx.saved_tensors
+                D_ = D.detach().cpu().numpy()
+                R_ = R.detach().cpu().numpy()
+                g_ = gamma.item()
+                E = torch.Tensor(compute_softdtw_backward(D_, R_, g_))
+                return E, None
